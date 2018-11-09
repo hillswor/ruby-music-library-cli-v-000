7a084f544df30327717d2ca8b8ab588110d7fad1
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-11-09 03:04:21 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -52,7 +52,7 @@ class MusicLibraryController
   def list_artists
     alphabetical_artist_list = Artist.all.sort_by {|artist| artist.name}
     alphabetical_artist_list.each_with_index do |artist, index|
-      puts "#{index + 1}. #{artist.name} - #{artist.songs} - #{artist.genre}"
+      puts "#{index + 1}. #{artist.name} - #{artist.songs} - #{artist.genres.name}"
     end
   end
 
