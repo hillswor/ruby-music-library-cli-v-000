66e9d3f82e2c687768563dcaaeabd5610c4adf99
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-11-09 04:09:22 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -66,7 +66,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     requested_artist = gets
-    song_list = Song.all.select {|song| song.artist.name == artist_name}
+    song_list = Song.all.select {|song| song.artist.name == requested_artist}
     song_list.sort_by! {|song| song.name}
     song_list.each_with_index do |song, index|
       puts "#{index + 1}. #{song.name} - #{song.genre.name}"
@@ -76,7 +76,7 @@ class MusicLibraryController
    def list_songs_by_genre
      puts "Please enter the name of a genre:"
      requested_genre = gets
-     song_list = Song.all.select {|song| song.genre.name == artist_name}
+     song_list = Song.all.select {|song| song.genre.name == requested_genre}
      song_list.sort_by! {|song| song.name}
      song_list.each_with_index do |song, index|
        puts "#{index + 1}. #{song.name} - #{song.genre.name}"
