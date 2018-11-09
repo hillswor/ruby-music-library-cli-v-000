7c8e0b74a0e140b0887a6b0f73087d57d9036d69
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-11-09 03:45:44 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -64,6 +64,8 @@ class MusicLibraryController
   end
 
   def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets
     requested_artist = Artist.find_by_name(input)
     requested_artist.songs.each_with_index do |song, index|
       puts "#{index + 1}. #{song.name} - #{song.genre}"
